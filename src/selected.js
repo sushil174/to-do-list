19c07ca9e6c3e86b266ddbf618a46b51600105d5
sushil174 <sushilkoregave123@gmail.com>
2024-10-31 02:55:09 +0530

    sidebar style change

--- src/selected.js
+++ src/selected.js
@@ -6,7 +6,7 @@ let select = (() => {
 	const due = document.querySelector('#due');
 	const week = document.querySelector('#week');
 
-	const shadow = 'inset 0.5em 0 #9F73AB';
+	const shadow = 'inset 0.3em 0 #9F73AB';
 
 	function clearAllShadows() {
 		const projects = Array.from(document.querySelectorAll('.project-card'));
